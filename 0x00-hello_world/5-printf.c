304c1651bb4032aa9ac9e7a1ab61210cb2ca9849
FelicityOsei <felicityosei@gmail.com>
2022-09-08 15:39:18 -0700

    faith

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,27 @@
+#include <studio.h>                                                                                                         
+
+                                                                                                                            
+
+/**                                                                                                                         
+
+* main - print the string in the put function                                                                               
+
+*                                                                                                                           
+
+* Description: using the main function                                                                                      
+
+* this program prints "Programming is like a multillingual puzzle                                                           
+
+* Return: 0                                                                                                                 
+
+*/                                                                                                                          
+
+int main(void)                                                                                                              
+
+{                                                                                                                           
+
+        printf("with proper grammar, but the outcome is a place of art,\n");                                                
+
+        return (0);                                                                                                         
+
+}           
