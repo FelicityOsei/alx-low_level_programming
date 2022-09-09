c00aa095915d9a0fceda7bef3ac884babface093
FelicityOsei <felicityosei@gmail.com>
2022-09-08 18:57:30 -0700

    great

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,27 +1,14 @@
-#include <studio.h>                                                                                                         
+#include <stdio.h>
 
-                                                                                                                            
-
-/**                                                                                                                         
-
-* main - print the string in the put function                                                                               
-
-*                                                                                                                           
-
-* Description: using the main function                                                                                      
-
-* this program prints "Programming is like a multillingual puzzle                                                           
-
-* Return: 0                                                                                                                 
-
-*/                                                                                                                          
-
-int main(void)                                                                                                              
-
-{                                                                                                                           
-
-        printf("with proper grammar, but the outcome is a place of art,\n");                                                
-
-        return (0);                                                                                                         
-
+/**                             
+* main - print the string in the put function                                  
+*                                                                              
+* Description: using the main function                                         
+* this program prints "Programming is like a multillingual puzzle              
+* Return: 0                                                                   
+*/                                                                             
+int main(void)                                                                 
+{
+printf("with proper grammar, but the outcome is a place of art,\n");   
+return (0);
 }           
