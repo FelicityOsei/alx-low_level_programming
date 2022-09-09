c4d922902bc6228b20ce82089fba5d0e1585e37c
FelicityOsei <felicityosei@gmail.com>
2022-09-08 17:35:02 -0700

    great

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
 #include <unistd.h>
 
-   /**
-    *main - print
-    *
-Return:Always 1 (Successful)
-   */
+
+
+/**
+ *
+ * *main - print
+ *
+ * *
+ *
+ * Return: Always 1 (successful)
+ *
+ * */
+
 int main(void)
+
 {
-  write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+
+	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+
 }
