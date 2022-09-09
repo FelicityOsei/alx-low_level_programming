7cb5ad4c90ffa87bb0a3d559be91bd3652c929e5
FelicityOsei <felicityosei@gmail.com>
2022-09-08 18:36:23 -0700

    great

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <studio.h>
 
 /**
- * main - Print
- * Return: Always 0 (successful)
- */
+* main - Print
+* Return: Always 0 (successful)
+*/
 int main(void)
 {
-	puts("\"Programming is the building of multillingual puzzle");
-	return (0);
+puts("\"Programming is the building of multillingual puzzle");
+return (0);
 }
