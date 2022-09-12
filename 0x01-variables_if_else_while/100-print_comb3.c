15fb33fef645c394bddc6e2c744a30c9e5cd7a99
FelicityOsei <felicityosei@gmail.com>
2022-09-12 16:38:29 -0700

    faith

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int p;
+int k;
+for (p = '0'; p < '9'; p++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (p == '0' && k < '1')
+continue;
+if (p == '1' && k < '2')
+continue;
+if (p == '2' && k < '3')
+continue;
+if (p == '3' && k < '4')
+continue;
+if (p == '4' && k < '5')
+continue;
+if (p == '5' && k < '6')
+continue;
+if (p == '6' && k < '7')
+continue;
+if (p == '7' && k < '8')
+continue;
+if (p == '8' && k < '9')
+continue;
+putchar(p);
+putchar(k);
+if (p == '8' && k == '9')
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
