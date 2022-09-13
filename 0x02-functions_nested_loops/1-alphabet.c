35d87a0be21246d25685a0b2bee4d10f02534422
FelicityOsei <felicityosei@gmail.com>
2022-09-13 13:21:05 -0700

    great

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+
+ * print_alphabeth - funtion to print abc
+
+ * void return back void
+
+ * Return: Always 0
+
+ */
+
+void print_alphabet(void)
+  
+{
+  
+  char e;
+  
+
+  
+  for (e = 'a'; e <= 'z'; e++)
+    
+    {
+      
+      _putchar (e);
+      
+    }
+  
+  _putchar('\n');
+  
+}
