1bd9ececd1406d2250e0cdb1981147a320e29ea0
FelicityOsei <felicityosei@gmail.com>
2022-09-14 14:12:42 -0700

    great

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,17 @@
-#include "main.h"
-/**
-* main - The program that return 0
-*
-* Return: Always 0 (success)
-*/
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+void times_table(void);
+void print_times_table(int n);
+void num(int r, int n);
+#endif
