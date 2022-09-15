867320d18155d438b6ce75ca690a35f66d5fa596
FelicityOsei <felicityosei@gmail.com>
2022-09-15 15:55:35 -0700

    great

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,23 +1,12 @@
 #include "main.h"
 /**
-* print_triangle - print backwards triangle of n size
-* @size: size of square
+* mul - multiplies two integers and returns the result
+*
+* @a: is the first number
+* @b: is the second number
+* Return: the product value of @a and @b
 */
-void print_triangle(int size)
+int mul(int a, int b)
 {
-int x, y, k, w, t;
-w = 1;
-t = size;
-if (size <= 0)
-_putchar('\n');
-for (y = 0; y < size; y++)
-{
-for (x = 0; x < (t - 1); x++)
-_putchar(' ');
-for (k = 0; k < w; k++)
-_putchar('#');
-_putchar('\n');
-t--;
-w++;
-}
+return (a * b);
 }
